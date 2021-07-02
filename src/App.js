ed6bc3e887633dd83615ca8c71f40aa44446a647
kulkov-a <kulkovplay@gmail.com>
2021-07-02 14:22:50 +0300

    Dop

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import Users from "./users/Users";
+import Posts from "./posts/Posts";
 
 function App() {
   return (
 
     <div>
 
+      <Users/>
 
+      <Posts/>
 
     </div>
 
---
+++ src/posts/Posts.js
@@ -0,0 +1,7 @@
+export default function Posts(props) {
+    return(
+        <div>
+
+        </div>
+    );
+}
