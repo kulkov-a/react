2c925877056bc146fcbfd1854966452e046c21a2
kulkov-a <kulkovplay@gmail.com>
2021-07-02 12:48:23 +0300

    HomeWork1 done

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,7 +1,12 @@
+
 export default function Menu(props) {
+    let {item} = props;
+
     return(
         <div>
+            {
 
+            }
         </div>
     );
 }
--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -1,5 +1,4 @@
 
-
 export default function Posts(props) {
     return(
         <div>
