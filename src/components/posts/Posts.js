df44278fdb0b53dd956b70f8b6eef6f096a532a1
kulkov-a <kulkovplay@gmail.com>
2021-07-02 13:11:52 +0300

    HomeWork1 done

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -1,10 +1,19 @@
+import {useEffect, useState} from "react";
 
 export default function Posts(props) {
 
+    let [posts, setPosts] = useState( [])
+
+    useEffect( () => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(value => value.json())
+            .then(postsFromServer)
+                setPosts(postsFromServer);
+    },   []);
 
     return(
         <div>
-
+            posts.map(post => <Post item={post}/>)
         </div>
     );
 }
